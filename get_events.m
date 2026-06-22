8aab077a79c55ad8129f44aa133e2138537c022c
ant-coh <antoine.cohic@gmail.com>
2026-06-22 10:28:53 -0400

    Fixed issue occuring with trials over 1min and 255 events

--- get_events.m
+++ get_events.m
@@ -1,15 +1,31 @@
 function [events] = get_events(c3d)
 
 events=struct();
+
 for i=1:c3d.parameters.EVENT.USED.DATA
-    if ~strcmp(c3d.parameters.EVENT.LABELS.DATA{i},'Foot Strike') && ~strcmp(c3d.parameters.EVENT.LABELS.DATA{i},'Foot Off')
+    idx=floor((i-1)/255)+1;
+    if idx==1
+        suffix='';
+    else
+        suffix=num2str(idx);
+    end
+    labels_field=['LABELS' suffix];
+    contexts_field=['CONTEXTS' suffix];
+    times_field=['TIMES' suffix];
+
+    local_i=mod(i-1,255)+1;
+    current_label=c3d.parameters.EVENT.(labels_field).DATA{local_i};
+    current_context=c3d.parameters.EVENT.(contexts_field).DATA{local_i};
+    current_time=c3d.parameters.EVENT.(times_field).DATA(2,local_i)+c3d.parameters.EVENT.(times_field).DATA(1,local_i)*60;
+
+    if ~strcmp(current_label,'Foot Strike') && ~strcmp(current_label,'Foot Off')
         continue
     end
-    label=strrep(sprintf('%s_%s',c3d.parameters.EVENT.CONTEXTS.DATA{i},c3d.parameters.EVENT.LABELS.DATA{i}),' ','_');
+    label=strrep(sprintf('%s_%s',current_context,current_label),' ','_');
     if ~isfield(events,label)
         events.(label)=[];
     end
-    events.(label)(1,length(events.(label))+1)=c3d.parameters.EVENT.TIMES.DATA(2,i);
+    events.(label)(1,end+1)=current_time;
 end
 
 end
