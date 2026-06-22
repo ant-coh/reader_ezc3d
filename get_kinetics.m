10c3aca5421cfcaf92534d1b48c4d4284a8cfcab
ant-coh <antoine.cohic@gmail.com>
2026-06-22 09:08:45 -0400

    Fixed typo and path to power unit

--- get_kinetics.m
+++ get_kinetics.m
@@ -4,16 +4,16 @@ kinetics=struct();
 
 labels=c3d.parameters.POINT.LABELS.DATA;
 for i=1:length(labels)
-    if countains(labels{i},'Force')
+    if contains(labels{i},'Force')
         kinetics.Forces.(labels{i})=squeeze(c3d.data.points(:,i,:))';
-    elseif countains(labels{i},'Moment')
+    elseif contains(labels{i},'Moment')
         kinetics.Moments.(labels{i})=squeeze(c3d.data.points(:,i,:))';
-    elseif countains(labels{i},'Power')
+    elseif contains(labels{i},'Power')
         kinetics.Powers.(labels{i})=squeeze(c3d.data.points(:,i,:))';
     end
 end
 kinetics.Forces.unit=c3d.parameters.POINT.FORCE_UNITS.DATA{1};
 kinetics.Moments.unit=c3d.parameters.POINT.MOMENT_UNITS.DATA{1};
-kinetics.Powers.unit=c3d.parameters.POINT.MOMENT_UNITS.DATA{1};
+kinetics.Powers.unit=c3d.parameters.POINT.POWER_UNITS.DATA{1};
 
 end
